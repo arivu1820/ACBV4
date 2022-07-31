bf26bbcaff50505e2eec9ffddac8bc12b8d6aeb9
PERARIVALAN <arivu1820@gmail.com>
2022-07-31 12:50:04 +0530

    delete

---
+++ app/src/main/java/com/futureinspirator/acbaradise/hello.java
@@ -0,0 +1,14 @@
+package com.futureinspirator.acbaradise;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hello extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+    }
+}
